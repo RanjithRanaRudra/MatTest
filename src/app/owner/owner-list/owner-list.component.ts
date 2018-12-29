30d357bdfda62eda5b58d571a8398a0e305e0efa
RanjithRanaRudra <srisasanka096@gmail.com>
2018-12-29 18:29:48 +0530

    commied by sasanka

--- src/app/owner/owner-list/owner-list.component.ts
+++ src/app/owner/owner-list/owner-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { PeriodicElement } from './../../interface/periodic-element.model';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-owner-list',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-list.component.scss']
 })
 export class OwnerListComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource: any;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit() {
+    this.getAllOwners();
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
+  public getAllOwners = () => {
+    /* this.repoService.getData('api/owner')
+    .subscribe(res => {
+      this.dataSource.data = res as Owner[];
+    }, (error) => {
+      this.errorservice.handleError(error);
+    }); */
+    const ELEMENT_DATA: PeriodicElement[] = [
+      {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+      {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+      {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+      {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+      {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+      {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+      {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+      {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+      {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+      {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    ];
 
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+  }
+  public doFilter = (filterValue: string) => {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
