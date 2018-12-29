6b56609804d3c37bcf6ed7cd6187140c51cd548c
RanjithRanaRudra <srisasanka096@gmail.com>
2018-12-29 17:35:13 +0530

    committed by sasanka 29-12-2018 17:35

--- src/app/Navigation/header/header.component.ts
+++ src/app/Navigation/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
-
+  username = 'SriSasanka';
   @Output() public sidenavToggle = new EventEmitter();
 
   ngOnInit() {
