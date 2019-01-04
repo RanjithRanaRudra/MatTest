1088c190feedc82cd33b2322da358c6a48417c18
RanjithRanaRudra <srisasanka096@gmail.com>
2019-01-04 22:54:09 +0530

    committed by sasanka at 04-01-2019 22:54

--- src/app/owner/owner-create/owner-create.component.ts
+++ src/app/owner/owner-create/owner-create.component.ts
@@ -1,4 +1,8 @@
+import { OwnerForCreation } from './../../interface/ownerForCreation.model';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-owner-create',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./owner-create.component.scss']
 })
 export class OwnerCreateComponent implements OnInit {
+  public ownerForm: FormGroup;
+  private dialogConfig;
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+    this.ownerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+      dateOfBirth: new FormControl(new Date()),
+      address: new FormControl('', [Validators.required, Validators.maxLength(100)])
+    });
+  }
+
+  public hasError = (controlName: string, errorName: string) => {
+    return this.ownerForm.controls[controlName].hasError(errorName);
+  }
+
+  public onCancel = () => {
+    this.location.back();
+  }
+
+  public createOwner = (ownerFormValue) => {
+    if (this.ownerForm.valid) {
+      this.executeOwnerCreation(ownerFormValue);
+    }
+  }
+
+  private executeOwnerCreation = (ownerFormValue) => {
+    const owner: OwnerForCreation = ownerFormValue;
   }
 
 }
